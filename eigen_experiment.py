41e524d34c5b779879356fee58258b52ba64b3e4
anna <ann21pike@gmail.com>
2022-09-26 13:03:07 +0300

    initial commit. contains LSQR, CG M-star algorithms and experimental settitngs

--- eigen_experiment.py
+++ eigen_experiment.py
@@ -46,7 +46,8 @@ def generate_tensor_eigen(m, p, eigen1, eigen2, funM, invM):
     A2 = np.matmul(A2.T, A2)
 
     A_hat = np.concatenate([A1.reshape(p, p, 1), A2.reshape(p, p, 1)], 2)
-    A_tensor = invM(A_hat)
+    fun_dct, inv_dct = generate_dct(2)
+    A_tensor = inv_dct(A_hat)
 
     X_true = np.random.randn(p, 1, 2)
     B = m_prod(A_tensor, X_true, funM, invM)
@@ -58,42 +59,42 @@ m, p = 1000, 10
 iters = 50
 path_to_save = '/home/anna/uni/thesis/numerical_results/'
 
-
-plt.figure(figsize=(15, 10))
-for i in range(4):
-    if i == 0:
-        M_random = 'DCT'
-        funM, invM = generate_dct(2)
-    else:
-        M_random = np.random.randint(1000)
-        funM, invM = generate_haar(2, random_state=M_random)
-
-    A_tensor, B, X_true = generate_tall_tensor_eigen(m, p, 1, 10**4, funM, invM)
-    X, error = algo.LSQR_mprod(A_tensor, B, funM, invM, iters, X_true=X_true)
-    bond_vector_f = np.vectorize(bound)
-    bound_vector = bond_vector_f(np.arange(iters+1), 2*10**4, error[0])
-
-
-    plt.subplot(2, 2, i+1)
-    plt.plot(error, c='b', label='eigvals of Ahat^TA_hat for first slice are 1-2, second slice: 1*10^9-2*10^9')
-    plt.plot(bound_vector, 'b--', label='bound')
-    plt.xlabel('iterations')
-    plt.yscale("log")
-
-    A_tensor, B, X_true = generate_tall_tensor_eigen(m, p, 1, 1, funM, invM)
-    X, error = algo.LSQR_mprod(A_tensor, B, funM, invM, iters, X_true=X_true)
-    bound_vector = bond_vector_f(np.arange(iters+1), 2, error[0])
-    plt.plot(error, c='y', label='all eigenvalues of Ahat^TA_hat are from 1 to 2')
-    plt.plot(bound_vector, 'y--', label='bound')
-    plt.xlabel('iterations')
-    plt.yscale("log")
-    plt.title(f'random state of M = {M_random}')
-    plt.ylabel('error')
-    plt.legend()
-plt.suptitle('M prod LSQR, A generated with DCT')
-plt.tight_layout()
-plt.savefig(path_to_save+'eigenvalues_experiment_LSQR_dct')
-plt.show()
+#
+# plt.figure(figsize=(15, 10))
+# for i in range(4):
+#     if i == 0:
+#         M_random = 'DCT'
+#         funM, invM = generate_dct(2)
+#     else:
+#         M_random = np.random.randint(1000)
+#         funM, invM = generate_haar(2, random_state=M_random)
+#
+#     A_tensor, B, X_true = generate_tall_tensor_eigen(m, p, 1, 10**4, funM, invM)
+#     X, error = algo.LSQR_mprod(A_tensor, B, funM, invM, iters, X_true=X_true)
+#     bond_vector_f = np.vectorize(bound)
+#     bound_vector = bond_vector_f(np.arange(iters+1), 2*10**4, error[0])
+#
+#
+#     plt.subplot(2, 2, i+1)
+#     plt.plot(error, c='b', label='eigvals of Ahat^TA_hat for first slice are 1-2, second slice: 1*10^9-2*10^9')
+#     plt.plot(bound_vector, 'b--', label='bound')
+#     plt.xlabel('iterations')
+#     plt.yscale("log")
+#
+#     A_tensor, B, X_true = generate_tall_tensor_eigen(m, p, 1, 1, funM, invM)
+#     X, error = algo.LSQR_mprod(A_tensor, B, funM, invM, iters, X_true=X_true)
+#     bound_vector = bond_vector_f(np.arange(iters+1), 2, error[0])
+#     plt.plot(error, c='y', label='all eigenvalues of Ahat^TA_hat are from 1 to 2')
+#     plt.plot(bound_vector, 'y--', label='bound')
+#     plt.xlabel('iterations')
+#     plt.yscale("log")
+#     plt.title(f'random state of M = {M_random}')
+#     plt.ylabel('error')
+#     plt.legend()
+# plt.suptitle('M prod LSQR, A generated with DCT')
+# plt.tight_layout()
+# plt.savefig(path_to_save+'eigenvalues_experiment_LSQR_dct')
+# plt.show()
 
 iters = 20
 plt.figure(figsize=(15, 10))
