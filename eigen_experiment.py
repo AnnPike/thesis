0437cc885e5b9df546f2d5cbf57b3eb1449a6051
anna <ann21pike@gmail.com>
2022-10-05 12:31:38 +0300

    initial commit. contains LSQR, CG M-star algorithms and experimental settitngs

--- eigen_experiment.py
+++ eigen_experiment.py
@@ -32,11 +32,8 @@ def generate_tall_tensor_eigen(m, p, eigen1, eigen2, funM, invM):
     A_hat = np.concatenate([A1.reshape(m, p, 1), A2.reshape(m, p, 1)], 2)
     fun_dct, inv_dct = generate_dct(2)
     A_tall_tensor = invM(A_hat)
-
-    X_true = np.random.randn(p, 1, 2)
-    B = m_prod(A_tall_tensor, X_true, funM, invM)
-
-    return A_tall_tensor, B, X_true
+    A_tall_tensor = fun_dct(A_tall_tensor)
+    return A_tall_tensor
 
 
 def generate_tensor_eigen(m, p, eigen1, eigen2, funM, invM):
@@ -46,29 +43,29 @@ def generate_tensor_eigen(m, p, eigen1, eigen2, funM, invM):
     A2 = np.matmul(A2.T, A2)
 
     A_hat = np.concatenate([A1.reshape(p, p, 1), A2.reshape(p, p, 1)], 2)
+    fun_dct, inv_dct = generate_dct(2)
     A_tensor = invM(A_hat)
-
-    X_true = np.random.randn(p, 1, 2)
-    B = m_prod(A_tensor, X_true, funM, invM)
-
-    return A_tensor, B, X_true
+    A_tensor = fun_dct(A_tensor)
+    return A_tensor
 
 
-m, p = 1000, 10
+m, p = 100, 10
 iters = 50
 path_to_save = '/home/anna/uni/thesis/numerical_results/'
 
+M_list = ['DCT', 21, 127, 333]
 
 plt.figure(figsize=(15, 10))
+X_true = np.random.randn(p, 1, 2)
 for i in range(4):
-    if i == 0:
-        M_random = 'DCT'
+    M_random = M_list[i]
+    if M_random == 'DCT':
         funM, invM = generate_dct(2)
     else:
-        M_random = np.random.randint(1000)
         funM, invM = generate_haar(2, random_state=M_random)
 
-    A_tensor, B, X_true = generate_tall_tensor_eigen(m, p, 1, 10**4, funM, invM)
+    A_tensor = generate_tall_tensor_eigen(m, p, 1, 10**4, funM, invM)
+    B = m_prod(A_tensor, X_true, funM, invM)
     X, error = algo.LSQR_mprod(A_tensor, B, funM, invM, iters, X_true=X_true)
     bond_vector_f = np.vectorize(bound)
     bound_vector = bond_vector_f(np.arange(iters+1), 2*10**4, error[0])
@@ -80,7 +77,8 @@ for i in range(4):
     plt.xlabel('iterations')
     plt.yscale("log")
 
-    A_tensor, B, X_true = generate_tall_tensor_eigen(m, p, 1, 1, funM, invM)
+    A_tensor = generate_tall_tensor_eigen(m, p, 1, 1, funM, invM)
+    B = m_prod(A_tensor, X_true, funM, invM)
     X, error = algo.LSQR_mprod(A_tensor, B, funM, invM, iters, X_true=X_true)
     bound_vector = bond_vector_f(np.arange(iters+1), 2, error[0])
     plt.plot(error, c='y', label='all eigenvalues of Ahat^TA_hat are from 1 to 2')
@@ -90,22 +88,24 @@ for i in range(4):
     plt.title(f'random state of M = {M_random}')
     plt.ylabel('error')
     plt.legend()
-plt.suptitle('M prod LSQR')
+plt.suptitle('M prod LSQR, A generated with M and additional application of DCT')
 plt.tight_layout()
-plt.savefig(path_to_save+'eigenvalues_experiment_LSQR')
+plt.savefig(path_to_save+'eigenvalues_experiment_LSQR_trans_dct')
 plt.show()
+
 
 iters = 20
 plt.figure(figsize=(15, 10))
 for i in range(4):
-    if i == 0:
-        M_random = 'DCT'
+    M_random = M_list[i]
+    if M_random == 'DCT':
         funM, invM = generate_dct(2)
     else:
-        M_random = np.random.randint(1000)
         funM, invM = generate_haar(2, random_state=M_random)
 
-    A_tensor, B, X_true = generate_tensor_eigen(m, p, 1, 10**4, funM, invM)
+    A_tensor = generate_tensor_eigen(m, p, 1, 10**4, funM, invM)
+
+    B = m_prod(A_tensor, X_true, funM, invM)
     X, error = algo.CG_M_tensor(A_tensor, B, funM, invM, iters, X_true=X_true)
     bond_vector_f = np.vectorize(bound)
     bound_vector = bond_vector_f(np.arange(iters+1), 2*10**4, error[0])
@@ -117,7 +117,8 @@ for i in range(4):
     plt.xlabel('iterations')
     plt.yscale("log")
 
-    A_tensor, B, X_true = generate_tensor_eigen(m, p, 1, 1, funM, invM)
+    A_tensor = generate_tensor_eigen(m, p, 1, 1, funM, invM)
+    B = m_prod(A_tensor, X_true, funM, invM)
     X, error = algo.CG_M_tensor(A_tensor, B, funM, invM, iters, X_true=X_true)
     bound_vector = bond_vector_f(np.arange(iters+1), 2, error[0])
     plt.plot(error, c='y', label='all eigenvalues of A^are from 1 to 2')
@@ -127,9 +128,9 @@ for i in range(4):
     plt.title(f'random state of M = {M_random}')
     plt.ylabel('error')
     plt.legend()
-plt.suptitle('M prod conjugate gradient')
+plt.suptitle('M prod conjugate gradient, A generated with M and additional application of DCT')
 plt.tight_layout()
-plt.savefig(path_to_save+'eigenvalues_experiment_CG')
+plt.savefig(path_to_save+'eigenvalues_experiment_CG_add_trans_dct')
 plt.show()
 
 link = 'https://www.dropbox.com/scl/fo/vwn07zx7omd0zvqw0laxs/h?dl=0&rlkey=oyllju7bignxhr1liqdqh8b23'
