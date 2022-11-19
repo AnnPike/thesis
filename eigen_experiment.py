416fb5f9b814f6c2e60504e368e963282d9a5d17
anna <ann21pike@gmail.com>
2022-11-19 20:04:11 +0200

    create plots for bounds

--- eigen_experiment.py
+++ eigen_experiment.py
@@ -7,13 +7,26 @@ import helper_plot
 import matplotlib.pyplot as plt
 import time
 
-def bound(i, k, E0):
-    return 2*E0*((np.sqrt(k)-1)/(np.sqrt(k)+1))**i
+
+def energy_norm(E, A_sym, funM, invM):
+    E_hat = funM(E)
+    A_hat = funM(A_sym)
+    nu_hat_sq = np.einsum('mpi,pli->mli', np.einsum('mpi,pli->mli', E_hat.transpose(1, 0, 2), A_hat), E_hat)
+    # print((nu_hat_sq<0).sum())
+    return nu_hat_sq.sum()
+
+# funM, invM = generate_haar(3, random_state=1)
+# E = np.random.randn(5, 10, 3)
+# A = np.random.rand(5, 5, 3)
+#
+# print(energy_norm(E, A, funM, invM))
+
+
+def bound(i, k, E0_norm):
+    return 2*E0_norm*((np.sqrt(k)-1)/(np.sqrt(k)+1))**i
 
 
 bond_vector_f = np.vectorize(bound)
-
-
 def nu_tensor_norm(E, A_tensor, funM, invM):
     return algo.tensor_frob_norm(algo.m_prod_three(E.transpose(1, 0, 2), A_tensor, E, funM, invM))
 
@@ -54,34 +67,7 @@ def generate_tall_A(A_hat, transform, funM, invM, X_true, B):
     return A_tall_tensor, B
 
 
-# def generate_tall_tensor_eigen(m, p, eigen1, eigen2, funM, invM, add_dct):
-#     np.random.seed(0)
-#     A1 = generate_tall_matrix(m, p, eigen1)
-#     A2 = generate_tall_matrix(m, p, eigen2)
-#
-#     A_hat = np.concatenate([A1.reshape(m, p, 1), A2.reshape(m, p, 1)], 2)
-#     fun_dct, inv_dct = generate_dct(2)
-#     A_tall_tensor = invM(A_hat)
-#     if add_dct:
-#         A_tall_tensor = fun_dct(A_tall_tensor)
-#     B = m_prod(A_tall_tensor, X_true, funM, invM)
-#     return A_tall_tensor, B
-#
-#
-# def generate_tensor_eigen(m, p, eigen1, eigen2, funM, invM, add_dct):
-#     np.random.seed(0)
-#     A1 = generate_tall_matrix(m, p, eigen1)
-#     A1 = np.matmul(A1.T, A1)
-#     A2 = generate_tall_matrix(m, p, eigen2)
-#     A2 = np.matmul(A2.T, A2)
-#
-#     A_hat = np.concatenate([A1.reshape(p, p, 1), A2.reshape(p, p, 1)], 2)
-#     fun_dct, inv_dct = generate_dct(2)
-#     A_tensor = invM(A_hat)
-#     if add_dct:
-#         A_tensor = fun_dct(A_tensor)
-#     B = m_prod(A_tensor, X_true, funM, invM)
-#     return A_tensor, B
+
 
 
 
@@ -133,9 +119,12 @@ def get_eigen_sym(A, funM):
     a2_max = np.round(lambda2.max(), 5)
     return a1_min, a1_max, a2_min, a2_max
 
-def get_eigen_tall(A, funM):
+def get_eigen(A, funM, sym_input):
     A_hat = funM(A)
-    A_hat_sym = np.einsum('mpi,pli->mli', A_hat.transpose(1, 0, 2), A_hat)
+    if not sym_input:
+        A_hat_sym = np.einsum('mpi,pli->mli', A_hat.transpose(1, 0, 2), A_hat)
+    else:
+        A_hat_sym = A_hat.copy()
     lambda1 = np.linalg.eigvals(A_hat_sym[:, :, 0])
     a1_min = np.round(lambda1.min(), 5)
     a1_max = np.round(lambda1.max(), 5)
@@ -146,8 +135,8 @@ def get_eigen_tall(A, funM):
     return a1_min, a1_max, a2_min, a2_max
 
 
-def calculate_cond(A, funM):
-    a1_min, a1_max, a2_min, a2_max = get_eigen_tall(A, funM)
+def calculate_cond(A, funM, sym_input):
+    a1_min, a1_max, a2_min, a2_max = get_eigen(A, funM, sym_input)
     k1 = a1_max/a1_min
     k2 = a2_max/a2_min
     return int(max(k1, k2))
@@ -169,7 +158,7 @@ def fill_dict_lines(dict_of_lines, A_tensor, B, funM, invM, error, list_of_X, pl
 path_to_save = '/home/anna/uni/thesis/numerical_results/eigen_n2_numerical/'
 np.random.seed(1)
 
-iters = 40
+iters = 10
 M_list = ['DCT', 21, 127, 333]
 
 # define parameters
@@ -180,85 +169,84 @@ degree = 6
 random_X = np.random.randn(p, 1, 2)
 
 start_time = time.time()
-for X_true in [None, random_X]:
-    if X_true is None:
-        B = np.random.randn(m, 1, 2)
-        plot_what_options = ['normalized residual', 'residual']
+X_true = random_X
+s = p*6
+
+np.random.seed(1)
+A_tall_hat_bad = generate_tall_A_hat(m, p, eigen1=1, eigen2=10**degree, k=k)
+np.random.seed(1)
+A_tall_hat_good = generate_tall_A_hat(m, p, eigen1=1, eigen2=1, k=k)
+
+plt.figure(figsize=(20,10))
+for i in range(4):
+    plt.subplot(2, 2, i+1)
+    M_random = M_list[i]
+
+    if M_random == 'DCT':
+        funM, invM = generate_dct(2)
     else:
-        B = None
-        plot_what_options = ['residual', 'error', 'normalized residual', 'normalized error']
+        funM, invM = generate_haar(2, random_state=M_random)
 
+    A_tensor, B = generate_tall_A(A_tall_hat_bad, 'original M', funM, invM, X_true, None)
+    list_of_X = algo.LSQR_mprod_tuples(A_tensor, B, funM, invM, iters)
+    list_of_E = [list_of_X[i]-X_true for i in range(iters+1)]
+    As = m_prod(A_tensor.transpose(1, 0, 2), A_tensor, funM, invM)
+    k = calculate_cond(As, funM, True)
+    error_vector = np.array([energy_norm(E, As, funM, invM) for E in list_of_E])
+    bound_vector = bond_vector_f(np.arange(iters+1), k, error_vector[0])
 
-    np.random.seed(1)
-    A_tall_hat_bad = generate_tall_A_hat(m, p, eigen1=1, eigen2=10**degree, k=k)
-    np.random.seed(1)
-    A_tall_hat_good = generate_tall_A_hat(m, p, eigen1=1, eigen2=1, k=k)
+    # plt.plot(error_vector, 'b')
+    # plt.plot(bound_vector, '--b')
+    #preconditioning
 
+    P, R = algo.blendenpick(A_tensor, funM, invM, s=s)
 
-    # genrating vectors to plot
+    list_of_X = algo.LSQR_mprod_tuples_precond(A_tensor, B, R, funM, invM, iters)
+    list_of_E = [list_of_X[i] - X_true for i in range(iters + 1)]
+    A_tensor_precond = m_prod(A_tensor, R, funM, invM)
+    Asp = m_prod(A_tensor_precond.transpose(1, 0, 2), A_tensor_precond, funM, invM)
+    k = calculate_cond(Asp, funM, True)
+    error_vector = np.array([energy_norm(E, Asp, funM, invM) for E in list_of_E])
+    error_vector_s = np.array([energy_norm(E, As, funM, invM) for E in list_of_E])
+    bound_vector = bond_vector_f(np.arange(iters + 1), k, error_vector[0])
+    bound_vector_s = bond_vector_f(np.arange(iters + 1), k, error_vector_s[0])
 
-    for plot_what in plot_what_options:
-        globals()[f'dict_of_lines_{plot_what}'] = {}
-    dict_of_cond = {}
+    # plt.plot(error_vector/error_vector[0], 'purple', alpha=0.5,label='error energy norm Asp')
+    # plt.plot(bound_vector/error_vector[0], 'purple', alpha=0.5,ls='--', label='bound Asp')
+    # plt.plot(error_vector_s/error_vector_s[0], 'purple',  label='error energy norm As')
+    # plt.plot(bound_vector_s/error_vector_s[0], 'purple', ls='--', label='bound As')
 
+    A_tensor, B = generate_tall_A(A_tall_hat_good, 'original M', funM, invM, X_true, None)
+    list_of_X = algo.LSQR_mprod_tuples(A_tensor, B, funM, invM, iters)
+    list_of_E = [list_of_X[i] - X_true for i in range(iters + 1)]
+    As = m_prod(A_tensor.transpose(1, 0, 2), A_tensor, funM, invM)
+    k = calculate_cond(As, funM, True)
+    error_vector = np.array([energy_norm(E, As, funM, invM) for E in list_of_E])
+    bound_vector = bond_vector_f(np.arange(iters + 1), k, error_vector[0])
 
+    # plt.plot(error_vector, 'y')
+    # plt.plot(bound_vector, 'y', ls='--')
 
-    for i in range(4):
+    P, R = algo.blendenpick(A_tensor, funM, invM, s=s)
+    list_of_X = algo.LSQR_mprod_tuples_precond(A_tensor, B, R, funM, invM, iters)
+    list_of_E = [list_of_X[i] - X_true for i in range(iters + 1)]
+    A_tensor_precond = m_prod(A_tensor, R, funM, invM)
+    Asp = m_prod(A_tensor_precond.transpose(1, 0, 2), A_tensor_precond, funM, invM)
+    k = calculate_cond(Asp, funM, True)
+    error_vector = np.array([energy_norm(E, Asp, funM, invM) for E in list_of_E])
+    error_vector_s = np.array([energy_norm(E, As, funM, invM) for E in list_of_E])
+    bound_vector = bond_vector_f(np.arange(iters + 1), k, error_vector[0])
+    bound_vector_s = bond_vector_f(np.arange(iters + 1), k, error_vector_s[0])
 
-        M_random = M_list[i]
-        dict_of_cond[i] = {}
-        for plot_what in plot_what_options:
-            globals()[f'dict_of_lines_{plot_what}'][i] = {}
-        if M_random == 'DCT':
-            funM, invM = generate_dct(2)
-        else:
-            funM, invM = generate_haar(2, random_state=M_random)
-        # A_tensor_bad = invM(A_tall_hat_bad)
-        # A_tensor_good = invM(A_tall_hat_good)
+    plt.plot(error_vector/error_vector[0], 'orange', alpha=0.5, label= 'energy norm Asp')
+    plt.plot(bound_vector/error_vector[0], 'orange', alpha=0.5, ls='--', label='bound Asp')
+    plt.plot(error_vector_s/error_vector_s[0], 'orange', label='energy norm As')
+    plt.plot(bound_vector_s/error_vector_s[0], 'orange', ls='--', label='bound As')
+    plt.legend()
+    plt.ylabel('normalized error')
 
-        A_tensor, B = generate_tall_A(A_tall_hat_bad, 'original M', funM, invM, X_true, B)
-        dict_of_cond[i]['orig bad'] = calculate_cond(A_tensor, funM)
-        X, error, list_of_X = algo.LSQR_mprod_tuples(A_tensor, B, funM, invM, iters, X_true=X_true)
-        for plot_what in plot_what_options:
-            fill_dict_lines(globals()[f'dict_of_lines_{plot_what}'], A_tensor, B, funM, invM, error, list_of_X, plot_what, 'orig bad')
-
-        #preconditioning
-        s = p*6
-
-        P, R = algo.blendenpick(A_tensor, funM, invM, s=s)
-        A_tensor_precond = m_prod(A_tensor, R, funM, invM)
-        dict_of_cond[i]['prec bad'] = calculate_cond(A_tensor_precond, funM)
-        X, error, list_of_X = algo.LSQR_mprod_tuples_precond(A_tensor, B, R, funM, invM, iters, X_true=X_true)
-        for plot_what in plot_what_options:
-            fill_dict_lines(globals()[f'dict_of_lines_{plot_what}'], A_tensor, B, funM, invM, error, list_of_X, plot_what,
-                            'prec bad')
-
-        A_tensor, B = generate_tall_A(A_tall_hat_good, 'original M', funM, invM, X_true, B)
-        dict_of_cond[i]['orig good'] = calculate_cond(A_tensor, funM)
-        X, error, list_of_X = algo.LSQR_mprod_tuples(A_tensor, B, funM, invM, iters, X_true=X_true)
-        for plot_what in plot_what_options:
-            fill_dict_lines(globals()[f'dict_of_lines_{plot_what}'], A_tensor, B, funM, invM, error, list_of_X, plot_what, 'orig good')
-
-
-        P, R = algo.blendenpick(A_tensor, funM, invM, s=s)
-        A_tensor_precond = m_prod(A_tensor, R, funM, invM)
-        dict_of_cond[i]['prec good'] = calculate_cond(A_tensor_precond, funM)
-        X, error, list_of_X = algo.LSQR_mprod_tuples_precond(A_tensor, B, R, funM, invM, iters, X_true=X_true)
-        for plot_what in plot_what_options:
-            fill_dict_lines(globals()[f'dict_of_lines_{plot_what}'], A_tensor, B, funM, invM, error, list_of_X, plot_what,
-                            'prec good')
-
-
-    for plot_what in plot_what_options:
-        helper_plot.plot_4M_2A_precond(M_list, globals()[f'dict_of_lines_{plot_what}'], plot_what, degree, m, p, s, dict_of_cond)
-        if X_true is None:
-            name = f'eigenvalues_experiment_LSQR_blendenpick_{m}_{p}_s{s}_k{k}_{plot_what.replace(" ", "_")}_B'
-        else:
-            name = f'eigenvalues_experiment_LSQR_blendenpick_{m}_{p}_s{s}_k{k}_{plot_what.replace(" ", "_")}_X_true'
-        plt.savefig(path_to_save + name)
-        plt.close()
-
-print(time.time()-start_time)
-
+    plt.yscale('log')
+plt.savefig(path_to_save+'bound_log')
+plt.show()
 
 
