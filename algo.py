fixed Cholesky_direct to work correctly for original space. inverse and cholesky decomposition utilize broadcast numpy linalg property

algo.py
@@ -2,7 +2,18 @@ import numpy as np
 from mprod import m_prod
 from scipy.fft import dct, idct, rfft, irfft
 from einsumt import einsumt as einsum #Multithreaded version of numpy.einsum function
+from joblib import Parallel, delayed
+import multiprocessing
+from scipy.linalg.lapack import dtrtri
+from scipy.linalg import solve_triangular
 
+
+
+num_cores = multiprocessing.cpu_count()
+
+
+def faceI(p, n):
+    return np.concatenate([np.expand_dims(np.identity(p), 2) for i in range(n)], 2)
 
 
 def transform(tensor, transform):
@@ -225,7 +236,7 @@ def LSQR_mprod_tuples_precond(A, C, R, funM, invM, itermax=25, tol=10 ** -5):
         U_wave = m_prod_three_fun(A, R, V) - m_prod_fun(U, alpha)
         U, beta = normalize(U_wave, funM, invM, tol)
 
-        V_wave = m_prod_three_fun(R.transpose(1,0,2), A.transpose(1,0,2), U) - m_prod_fun(V, beta)
+        V_wave = m_prod_three_fun(R.transpose(1, 0, 2), A.transpose(1,0,2), U) - m_prod_fun(V, beta)
         V, alpha = normalize(V_wave, funM, invM, tol) # V - l*s*p
 
         # orthogonal transformation
@@ -247,10 +258,20 @@ def LSQR_mprod_tuples_precond(A, C, R, funM, invM, itermax=25, tol=10 ** -5):
     return list_of_X
 
 
+
+# def inverse_tri_mat(mat, lower):
+#     inv_mat, _ = dtrtri(mat, lower=lower)
+#     return inv_mat
+
+
+def inverse_tri_mat(mat, lower):
+    inv_mat = solve_triangular(mat, np.eye(len(mat)), lower=lower)
+    return inv_mat
+
+
 def inverse_tensor(tensor, funM=None, invM=None):
     tensor_hat = transform(tensor, funM)
-    m, p, n = tensor_hat.shape
-    face_inv = np.concatenate([np.linalg.inv(tensor_hat[:, :, i]).reshape(m, p, 1) for i in range(n)], 2)
+    face_inv = np.linalg.inv(tensor_hat.transpose(2, 0, 1)).transpose(1, 2, 0)
     tensor_inv_out = transform(face_inv, invM)
     return tensor_inv_out
 
@@ -281,23 +302,35 @@ def tensor_QR(tensor, funM=None, invM=None):
     return tensor_Q, tensor_R
 
 
-def tensor_Cholesky(tensor, funM=None, invM=None):
+# def tensor_Cholesky(tensor, reg=0, funM=None, invM=None):
+#     m, p, n = tensor.shape
+#     tensor_hat = transform(tensor, funM)
+#     gram_ten = einsum('mpi,pli->mli', tensor_hat.transpose(1, 0, 2), tensor_hat)
+#     gram_ten_reg = gram_ten + reg*faceI(p, n)
+#     tensor_L = np.empty((p, p, 0))
+#     for i in range(n):
+#         L = np.linalg.cholesky(gram_ten_reg[:, :, i])
+#         tensor_L = np.concatenate([tensor_L, np.expand_dims(L, -1)], 2)
+#     tensor_L_out = transform(tensor_L, invM)
+#     return tensor_L_out
+
+
+
+def tensor_Cholesky(tensor, reg=0, funM=None, invM=None):
     m, p, n = tensor.shape
     tensor_hat = transform(tensor, funM)
     gram_ten = einsum('mpi,pli->mli', tensor_hat.transpose(1, 0, 2), tensor_hat)
-    tensor_L = np.empty((p, p, 0))
-    for i in range(n):
-        L = np.linalg.cholesky(gram_ten[:, :, i])
-        tensor_L = np.concatenate([tensor_L, np.expand_dims(L, -1)], 2)
+    gram_ten_reg = gram_ten + reg*faceI(p, n)
+    tensor_L = np.linalg.cholesky(gram_ten_reg.transpose(2, 0, 1)).transpose(1, 2, 0)
     tensor_L_out = transform(tensor_L, invM)
     return tensor_L_out
 
 
-def Cholesky_direct(tenA, omatB, funM=None, invM=None):
+def Cholesky_direct(tenA, omatB, reg=0, funM=None, invM=None):
     tensorA_hat = transform(tenA, funM)
     omatB_hat = transform(omatB, funM)
-    tenL_hat = tensor_Cholesky(tensorA_hat, funM, invM)
-    tenL_hat_inv = inverse_tensor(tenL_hat, funM, invM)
+    tenL_hat = tensor_Cholesky(tensorA_hat, reg)
+    tenL_hat_inv = inverse_tensor(tenL_hat)
     omatX_hat1 = facewise_mult(tensorA_hat.transpose(1, 0, 2), omatB_hat)
     omatX_hat2 = facewise_mult(tenL_hat_inv, omatX_hat1)
     omatX_hat = facewise_mult(tenL_hat_inv.transpose(1, 0, 2), omatX_hat2)

check_algo.py
@@ -3,18 +3,30 @@ import random_generation as rg
 import numpy as np
 from mprod import generate_haar, generate_dct, m_prod
 
-m, p, n = 200, 10, 5
+m, p, n = 2000, 100, 300
+funM, invM = generate_haar(n, 21)
+np.random.seed(21)
 
-tenA_hat = rg.generate_tensor(m, p, n, 'coherent')
-omatX_hat = np.random.randn(p, 1, n)
+tenA = rg.generate_tensor(m, p, n, 'coherent')
+omatX = np.random.randn(p, 1, n)
 
-omatNoise = np.random.randn(m, 1, n)
-# omatNoise = np.zeros((m, 1, n))
-omatB_hat = algo.facewise_mult(tenA_hat, omatX_hat) + omatNoise
+# omatNoise = 0.1*np.random.randn(m, 1, n)
+omatNoise = np.zeros((m, 1, n))
+omatB = algo.m_prod(tenA, omatX, funM, invM) + omatNoise
 
 
-X_sol_hat = algo.Cholesky_direct(tenA_hat, omatB_hat)
-print(algo.tensor_frob_norm(X_sol_hat-omatX_hat))
 
-funM, invM = generate_haar(n, random_state=2)
-print(algo.tensor_frob_norm(invM(X_sol_hat)-invM(omatX_hat)))
+# import time
+# start_time = time.time()
+
+X_sol_hat = algo.Cholesky_direct(funM(tenA), funM(omatB))
+print(algo.tensor_frob_norm(invM(X_sol_hat)-omatX))
+print(algo.tensor_frob_norm(X_sol_hat-funM(omatX)))
+
+X_sol = algo.Cholesky_direct(tenA, omatB, funM=funM, invM=invM)
+
+# print('it took:', time.time()-start_time)
+
+print(algo.tensor_frob_norm(X_sol-omatX))
+print(algo.tensor_frob_norm(funM(X_sol)-funM(omatX)))
+

compare_real_dts.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import algo
 import pickle
 from matplotlib.pyplot import cm
+from mprod import generate_haar, generate_dct
+
 
 tenX = pickle.load(open('datasets/rainfall_ten', 'rb'))
 print(tenX.shape)
@@ -17,60 +19,42 @@ for i in range(n):
        plt.plot(tenA[-1, :, i], c=colors[i])
        plt.scatter(p, omatB[-1, :, i], c=colors[i])
 plt.show()
-
-
-
-
-
 print(tenA.shape, omatB.shape)
 
 tenA_norm = (tenA - np.expand_dims(tenA.mean(0), 0))
 omatB_norm = (omatB - np.expand_dims(omatB.mean(0), 0))
 tenAbias = np.concatenate((np.ones((m, 1, n)), tenA_norm), 1)
 
-X_pred = algo.Cholesky_direct(tenA_norm, omatB_norm)
-plt.plot(X_pred[:, 0, :])
-plt.show()
-identity_error = algo.tensor_frob_norm(algo.facewise_mult(tenA_norm, X_pred)-omatB_norm)
+X_pred_identity = algo.Cholesky_direct(tenA_norm, omatB_norm, reg=10)
+identity_error = algo.tensor_frob_norm(algo.facewise_mult(tenA_norm, X_pred_identity)-omatB_norm)
 print(identity_error)
 
 
-from mprod import generate_haar, generate_dct
+plt.figure(figsize=(15, 10))
+plt.subplot(3, 2, 1)
+plt.plot(X_pred_identity[:, 0, :].T)
+plt.title(f'original solution, error = {np.round(identity_error, 2)}')
 
-success_count = 0
-fail_count = 0
-for i in range(10**4):
+found = 0
+i = 0
+while found < 2:
        funM, invM = generate_haar(n, random_state=i)
-
-       tenAhat = funM(tenA)
-       tenAhat_norm = tenAhat - np.expand_dims(tenAhat.mean(0), 0)
-       # tenAbias = np.concatenate((invM(np.ones((m, 1, n))), invM(tenA_norm_hat)), 1)
-
-       omatBhat = funM(omatB)
-       omatBhat_norm = (omatBhat - np.expand_dims(omatBhat.mean(0), 0))
-
-       X_pred_hat = algo.Cholesky_direct(tenAhat_norm, omatBhat_norm)
-       error_hat = algo.tensor_frob_norm(algo.facewise_mult(tenAhat_norm, X_pred_hat)-omatBhat_norm)
-       # print(error_hat)
-       error = algo.tensor_frob_norm(algo.m_prod(invM(tenAhat_norm), invM(X_pred_hat), funM, invM) - invM(omatBhat_norm))
-       # print(error)
+       # tenAhat_norm = funM(tenA_norm)
+       # omatBhat_norm = funM(omatB_norm)
+       X_pred = algo.Cholesky_direct(tenA_norm, omatB_norm, reg=10, funM=funM, invM=invM)
+       error_hat = algo.tensor_frob_norm(algo.facewise_mult(funM(tenA_norm), funM(X_pred))-funM(omatB_norm))
+       error = algo.tensor_frob_norm(algo.m_prod(tenA_norm, X_pred, funM, invM) - omatB_norm)
 #
-       if error < identity_error:
-#               # print(i, error, 'SUCCESS')
-#               success_count+=1
-#        else:
-#               print(identity_error-error_hat)
-#               fail_count+=1
-# print(success_count/10**4, fail_count/10**4)
+       if error < 0.98*identity_error:
+           found+=1
+           print(found)
+           plt.subplot(3, 2, 2*found+1)
+           plt.plot(invM(X_pred)[:, 0, :].T)
+           plt.title(f'orig space, error = {np.round(error, 2)}')
+           plt.subplot(3, 2, 2*found+2)
+           plt.plot(funM(X_pred)[:, 0, :].T)
+           plt.title(f'hat space, random state={i}')
+       i += 1
 
-              plt.figure(figsize=(15, 5))
-              plt.subplot(121)
-              plt.plot(X_pred[:, 0, :])
-              plt.title('orig space')
-
-              plt.subplot(122)
-              plt.plot(funM(X_pred)[:, 0, :])
-              plt.title('hat space')
-              plt.suptitle(f'random state funM = {i}, error = {np.round(error, 1)}')
-              plt.show()
-       #
+plt.tight_layout()
+plt.show()