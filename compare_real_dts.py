fixed Cholesky_direct to work correctly for original space. inverse and cholesky decomposition utilize broadcast numpy linalg property

compare_real_dts.py
@@ -7,7 +7,7 @@ from matplotlib.pyplot import cm
 from mprod import generate_haar, generate_dct
 
 
-tenX = pickle.load(open('datasets/rainfall_ten', 'rb'))
+tenX = pickle.load(open('datasets/rainfall_ten', 'rb')).transpose(2, 1, 0)
 print(tenX.shape)
 
 tenA = tenX[:, :-2, :]
@@ -25,7 +25,7 @@ tenA_norm = (tenA - np.expand_dims(tenA.mean(0), 0))
 omatB_norm = (omatB - np.expand_dims(omatB.mean(0), 0))
 tenAbias = np.concatenate((np.ones((m, 1, n)), tenA_norm), 1)
 
-X_pred_identity = algo.Cholesky_direct(tenA_norm, omatB_norm, reg=10)
+X_pred_identity = algo.Cholesky_direct(tenA_norm, omatB_norm, reg=0.1)
 identity_error = algo.tensor_frob_norm(algo.facewise_mult(tenA_norm, X_pred_identity)-omatB_norm)
 print(identity_error)
 
@@ -41,11 +41,11 @@ while found < 2:
        funM, invM = generate_haar(n, random_state=i)
        # tenAhat_norm = funM(tenA_norm)
        # omatBhat_norm = funM(omatB_norm)
-       X_pred = algo.Cholesky_direct(tenA_norm, omatB_norm, reg=10, funM=funM, invM=invM)
+       X_pred = algo.Cholesky_direct(tenA_norm, omatB_norm, reg=0.1, funM=funM, invM=invM)
        error_hat = algo.tensor_frob_norm(algo.facewise_mult(funM(tenA_norm), funM(X_pred))-funM(omatB_norm))
        error = algo.tensor_frob_norm(algo.m_prod(tenA_norm, X_pred, funM, invM) - omatB_norm)
 #
-       if error < 0.98*identity_error:
+       if error < 0.99*identity_error:
            found+=1
            print(found)
            plt.subplot(3, 2, 2*found+1)
@@ -57,4 +57,5 @@ while found < 2:
        i += 1
 
 plt.tight_layout()
+plt.savefig('datasets/rainfall_figure_year')
 plt.show()