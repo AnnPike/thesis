fixed Cholesky_direct to work correctly for original space. inverse and cholesky decomposition utilize broadcast numpy linalg property

compare_real_dts.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 import algo
 import pickle
 from matplotlib.pyplot import cm
-from mprod import generate_haar, generate_dct
+from mprod import generate_haar, generate_dct, x_m3
+from numpy import linalg as LA
 
 
-tenX = pickle.load(open('datasets/rainfall_ten', 'rb')).transpose(2, 1, 0)
+R = 1
+tenX = pickle.load(open('datasets/rainfall_ten', 'rb'))
 print(tenX.shape)
 
 tenA = tenX[:, :-2, :]
@@ -25,7 +27,7 @@ tenA_norm = (tenA - np.expand_dims(tenA.mean(0), 0))
 omatB_norm = (omatB - np.expand_dims(omatB.mean(0), 0))
 tenAbias = np.concatenate((np.ones((m, 1, n)), tenA_norm), 1)
 
-X_pred_identity = algo.Cholesky_direct(tenA_norm, omatB_norm, reg=0.1)
+X_pred_identity = algo.Cholesky_direct(tenA_norm, omatB_norm, reg=R)
 identity_error = algo.tensor_frob_norm(algo.facewise_mult(tenA_norm, X_pred_identity)-omatB_norm)
 print(identity_error)
 
@@ -33,29 +35,48 @@ print(identity_error)
 plt.figure(figsize=(15, 10))
 plt.subplot(3, 2, 1)
 plt.plot(X_pred_identity[:, 0, :].T)
-plt.title(f'original solution, error = {np.round(identity_error, 2)}')
+plt.title(f'original solution, residual = {np.round(identity_error, 2)}')
 
-found = 0
-i = 0
-while found < 2:
-       funM, invM = generate_haar(n, random_state=i)
-       # tenAhat_norm = funM(tenA_norm)
-       # omatBhat_norm = funM(omatB_norm)
-       X_pred = algo.Cholesky_direct(tenA_norm, omatB_norm, reg=0.1, funM=funM, invM=invM)
-       error_hat = algo.tensor_frob_norm(algo.facewise_mult(funM(tenA_norm), funM(X_pred))-funM(omatB_norm))
-       error = algo.tensor_frob_norm(algo.m_prod(tenA_norm, X_pred, funM, invM) - omatB_norm)
-#
-       if error < 0.99*identity_error:
-           found+=1
-           print(found)
-           plt.subplot(3, 2, 2*found+1)
-           plt.plot(invM(X_pred)[:, 0, :].T)
-           plt.title(f'orig space, error = {np.round(error, 2)}')
-           plt.subplot(3, 2, 2*found+2)
-           plt.plot(funM(X_pred)[:, 0, :].T)
-           plt.title(f'hat space, random state={i}')
-       i += 1
+
+
+funM, invM = generate_dct(n)
+# tenAhat_norm = funM(tenA_norm)
+# omatBhat_norm = funM(omatB_norm)
+X_pred = algo.Cholesky_direct(tenA_norm, omatB_norm, reg=R, funM=funM, invM=invM)
+error_hat = algo.tensor_frob_norm(algo.facewise_mult(funM(tenA_norm), funM(X_pred))-funM(omatB_norm))
+error = algo.tensor_frob_norm(algo.m_prod(tenA_norm, X_pred, funM, invM) - omatB_norm)
+
+plt.subplot(3, 2, 3)
+plt.plot(invM(X_pred)[:, 0, :].T)
+plt.title(f'orig space, residual = {np.round(error, 2)}')
+plt.subplot(3, 2, 4)
+plt.plot(funM(X_pred)[:, 0, :].T)
+plt.title(f'hat space, M = DCT')
+
+def generate_corr(ten):
+    m, p, n = ten.shape
+    X = ten.sum(1)
+    X_cent = (X - X.mean(0))/X.std(0)
+    X_corr = X_cent.T @ X_cent/m
+    eigenvalues, eigenvectors = LA.eig(X_corr)
+    return x_m3(eigenvectors.T), x_m3(eigenvectors)
+
+
+funM, invM = generate_corr(tenA)
+
+X_pred = algo.Cholesky_direct(tenA_norm, omatB_norm, reg=R, funM=funM, invM=invM)
+error_hat = algo.tensor_frob_norm(algo.facewise_mult(funM(tenA_norm), funM(X_pred))-funM(omatB_norm))
+error = algo.tensor_frob_norm(algo.m_prod(tenA_norm, X_pred, funM, invM) - omatB_norm)
+print(np.isclose(error_hat, error))
+
+plt.subplot(3, 2, 5)
+plt.plot(invM(X_pred)[:, 0, :].T)
+plt.title(f'orig space, residual = {np.round(error, 2)}')
+plt.subplot(3, 2, 6)
+plt.plot(funM(X_pred)[:, 0, :].T)
+plt.title(f'hat space, M = eigenvectors of cov mat')
 
 plt.tight_layout()
-plt.savefig('datasets/rainfall_figure_year')
+plt.savefig('datasets/rainfall_figure')
 plt.show()
+